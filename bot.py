83cc45584771b8858e951de3a7a92b855d5b34ce
Karel <Karel.Peeters@ciminko.lu>
2019-03-28 18:43:00 +0100

    potential unicode fix

--- bot.py
+++ bot.py
@@ -596,7 +596,7 @@ async def motions():
     motions = {}
     with open("motions.txt", 'rb') as file:
         for line in file:
-            motions[str(line[1:].split(' | ')[0])] = line
+            motions[str(line[1:].split(' | ')[0])] = line.decode('UTF-8')
     if len(motions) == 0:
         await bot.say('There are no standing motions right now')
     allMotions = ""
